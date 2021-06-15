efb8201f38ff84c34115811dc19cc9d65d9e56a9
KJH <bkho@naver.com>
2021-06-15 17:37:01 +0900

    성경용 autocomplete 분리

---
+++ src/Layout/autoCompleteBible.js
@@ -0,0 +1,68 @@
+import { makeStyles, TextField } from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from 'react'
+import Http from '../Utils/Http';
+
+/**
+ * @typedef AutoCompleteBIbleClasses
+ * @property {Object} 
+ */
+/**
+ * @typedef AutoCompleteProperties
+ * @property {Function} onChange
+ * @property {ClassNameMap} classes
+ * @property {boolean} fullWidth
+ * @property {String} id
+ * @property {boolean} validator
+ * @property {Function} renderOption
+ * @property {Function} autocompleteTextfieldRender
+ */
+/**
+ * @param {AutoCompleteProperties} props 
+ * @returns 
+ */
+export default function AutoCompleteBible(props) {
+
+  const http = Http();
+  const { onChange, classes, fullWidth, validator, className } = props;
+  const id = props.id || "bible_auto_complete";
+  const renderOption = props.renderOption || ((params) => (<><span className={classes.shortName}>{params.short_name}</span>{params.bible_name}</>))
+  const autocompleteTextfieldRender = props.autocompleteTextfieldRender || 
+    function autocompleteTextfieldRender(params) {
+    return (
+      <TextField 
+        key={params.bible_code} 
+        label="성경" {...params} 
+        fullWidth={fullWidth} 
+        // className={classes.autocomplete_textfield} 
+        variant="outlined" 
+        required
+        error={validator}
+        helperText={validator ? '성경을 선택해주세요' : ''}
+      />
+    )
+  }
+
+  const [bibleCodes, setBibleCodes] = React.useState([]);
+  React.useEffect(async () => {
+    var response = await http.get({query : "/resource/bible"});
+    var recvBibleCodes = response.data;
+    setBibleCodes(recvBibleCodes)
+  }, [])
+
+  
+  return (
+    bibleCodes.length > 0 ? 
+    (<Autocomplete
+      id={id}
+      options={bibleCodes}
+      getOptionLabel={(props) => props.bible_name}
+      onChange={onChange}
+      fullWidth={fullWidth}
+      className={className}
+      renderOption={renderOption}
+      renderInput={autocompleteTextfieldRender}
+    ></Autocomplete>)
+    : <></>
+  )
+}
--- src/Layout/cardTemplate.js
+++ src/Layout/cardTemplate.js
@@ -2,6 +2,7 @@ import { Box, Button, Container, Divider, makeStyles, Paper, TextField } from '@
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import React from 'react'
 import Http from '../Utils/Http';
+import AutoCompleteBible from './autoCompleteBible';
 
 export default function CardTemplateComponent(props) {
 
@@ -22,7 +23,7 @@ export default function CardTemplateComponent(props) {
     },
     content_teamplte: {
       display: 'flex',
-      flexWrap : 'wrap',
+      flexWrap: 'wrap',
       flexDirection: 'column',
       padding: '10px',
       '& .MuiTextField-root': {
@@ -65,14 +66,6 @@ export default function CardTemplateComponent(props) {
   }));
   const classes = useStyle();
   const { history } = props;
-  const [bibleCodes, setBibleCodes] = React.useState([]);
-  
-  React.useEffect(async () => {
-
-    var response = await http.get({query : "/resource/bible"});
-    var recvBibleCodes = response.data;
-    setBibleCodes(recvBibleCodes)
-  }, [])
 
 
   const [value, setValue] = React.useState({
@@ -87,12 +80,8 @@ export default function CardTemplateComponent(props) {
     checkValidate();
   }, [value])
   React.useEffect(() => {
-    if(value.bible_code && value.chapter && value.f_verse) {
+    if (value.bible_code && value.chapter && value.f_verse) {
       loadContent()
-    }
-    if(value.bible_code) {
-      var bibleInfo = bibleCodes.find((item) => item.bible_code == value.bible_code);
-      setOption({...option, maxChapter : bibleInfo.chapter});
     }
   }, [value.bible_code, value.chapter, value.f_verse, value.l_verse])
 
@@ -106,7 +95,7 @@ export default function CardTemplateComponent(props) {
   })
   const [option, setOption] = React.useState({
     maxChapter: 999,
-    edited : false
+    edited: false
   })
 
   const goBack = () => {
@@ -116,39 +105,27 @@ export default function CardTemplateComponent(props) {
     setValue({ ...value, [props]: event.target.value });
   }
   const onChangeAutocomplete = function (event, newValue) {
-    setValue({...value, bible_code : newValue.bible_code})
-  }
-  function autocompleteTextfieldRender(params) {
-    return (
-      <TextField 
-        key={params.bible_code} 
-        label="성경" {...params} 
-        fullWidth={false} 
-        className={classes.autocomplete_textfield} 
-        variant="outlined" 
-        required
-        error={validators.bible_code}
-        helperText={validators.bible_code ? '성경을 선택해주세요' : ''}/>
-    )
+    setValue({ ...value, bible_code: newValue.bible_code })
+    setOption({ ...option, maxChapter: newValue.chapter });
   }
   async function loadContent() {
     try {
       var { bible_code, chapter, f_verse, l_verse } = value;
 
       var queryData = { bible_code, chapter, f_verse };
-      if(l_verse) {
+      if (l_verse) {
         queryData.l_verse = l_verse;
       }
-      var res = await http.get({ query : 'RC/oyo/content', data : queryData})
+      var res = await http.get({ query: 'RC/oyo/content', data: queryData })
       var content = res.data;
-      setValue({...value, content : content});
+      setValue({ ...value, content: content });
     } catch (error) {
       console.error(error);
     }
   }
   function onSaveHandling() {
     try {
-      var result = http.post({query: "RC", data: value})
+      var result = http.post({ query: "RC", data: value })
 
       /********** Notice Saved OYO Card **********/
 
@@ -175,104 +152,98 @@ export default function CardTemplateComponent(props) {
   }
   const checkValidate = () => {
     var validated = {}
-    for(var itr in value) {
-      switch(itr) {
+    for (var itr in value) {
+      switch (itr) {
         case 'f_verse':
         case 'chapter':
         case 'bible_code':
         case 'content':
-          if(!value[itr]) {
+          if (!value[itr]) {
             validated[itr] = true;
-          } else if(validators[itr]){
+          } else if (validators[itr]) {
             validated[itr] = false;
           }
           break;
       }
     }
-    setValidators({...validators, ...validated})
+    setValidators({ ...validators, ...validated })
   }
-  
-  return (
-    <>
-      { bibleCodes.length > 0 ?
-        <Container maxWidth="sm" className={classes.root_template}>
-          <div className={classes.content_wrap}>
-            <Paper elevation={2} variant="outlined" className={classes.content_teamplte}>
-              <Box component="h3" className={classes.head_title}>사용자 구절(OYO) 추가</Box>
-              <TextField
-                id="teamplte_theme"
-                type="text"
-                label="암송 주제"
-                value={value.theme}
-                variant="outlined"
-                onChange={onChangeHandling('theme')}
-              />
-              <div className={classes.row_field}>
-                <Autocomplete
-                  id="teamplte_bible"
-                  options={bibleCodes}
-                  getOptionLabel={(props) => props.bible_name}
-                  onChange={onChangeAutocomplete}
-                  fullWidth={false}
-                  renderOption={(params) => (<><span className={classes.shortName}>{params.short_name}</span>{params.bible_name}</>)}
-                  renderInput={autocompleteTextfieldRender}
-                ></Autocomplete>
-                <TextField
-                  id="template_chapter"
-                  label="장"
-                  type="number"
-                  value={value.chapter}
-                  variant="outlined"
-                  required
-                  onChange={onChangeHandling('chapter')}
-                  InputProps={{ inputProps: { min : 0, max : option.maxChapter}}}
-                  error={validators.chapter}
-                />
-              </div>
-              <div className={classes.row_field}>
-                <TextField
-                  id="template_fverse"
-                  type="number"
-                  label="시작 절"
-                  value={value.f_verse}
-                  variant="outlined"
-                  required
-                  onChange={onChangeHandling('f_verse')}
-                  error={validators.f_verse}
-                />
-                <Box component="h4" className={classes.tilde}>~</Box>
-                <TextField
-                  id="template_lverse"
-                  type="number"
-                  label="끝 절"
-                  value={value.l_verse}
-                  variant="outlined"
-                  onChange={onChangeHandling('l_verse')}
-                />
-              </div>
-              <TextField
-                id="teamplte_content"
-                type="text"
-                label="내용을 입력해주세요."
-                value={value.content}
-                variant="outlined"
-                multiline
-                rows="5"
-                required
-                onChange={onChangeHandling('content')} 
-                error={validators.content}
-                helperText={validators.content ? '내용을 입력하거나 확인해주세요' : ''}
-              />
-              <Divider />
-              <div className={classes.action_buttons}>
-                <Button type="button" color="secondary" aria-label="oyo-cancel" variant="outlined" onClick={goBack}>취소</Button>
-                <Button type="button" color="primary" aria-label="oyo-write" variant="contained" onClick={onSaveHandling}>저장</Button>
-              </div>
-            </Paper>
-          </div>
-        </Container>
 
-        : <></>}
-    </>
+  return (
+    <Container maxWidth="sm" className={classes.root_template}>
+      <div className={classes.content_wrap}>
+        <Paper elevation={2} variant="outlined" className={classes.content_teamplte}>
+          <Box component="h3" className={classes.head_title}>사용자 구절(OYO) 추가</Box>
+          <TextField
+            id="teamplte_theme"
+            type="text"
+            label="암송 주제"
+            value={value.theme}
+            variant="outlined"
+            onChange={onChangeHandling('theme')}
+          />
+          <div className={classes.row_field}>
+            <AutoCompleteBible
+              id="template_bible"
+              classes={classes}
+              fullWidth={false}
+              onChange={onChangeAutocomplete}
+              renderOption={(params) => (<><span className={classes.shortName}>{params.short_name}</span>{params.bible_name}</>)}
+            />
+            <TextField
+              id="template_chapter"
+              label="장"
+              type="number"
+              value={value.chapter}
+              variant="outlined"
+              required
+              onChange={onChangeHandling('chapter')}
+              InputProps={{ inputProps: { min: 0, max: option.maxChapter } }}
+              error={validators.chapter}
+            />
+          </div>
+          <div className={classes.row_field}>
+            <TextField
+              id="template_fverse"
+              type="number"
+              label="시작 절"
+              value={value.f_verse}
+              variant="outlined"
+              required
+              onChange={onChangeHandling('f_verse')}
+              error={validators.f_verse}
+            />
+            <Box component="h4" className={classes.tilde}>~</Box>
+            <TextField
+              id="template_lverse"
+              type="number"
+              label="끝 절"
+              value={value.l_verse}
+              variant="outlined"
+              onChange={onChangeHandling('l_verse')}
+            />
+          </div>
+          <TextField
+            id="teamplte_content"
+            type="text"
+            label="내용을 입력해주세요."
+            value={value.content}
+            variant="outlined"
+            multiline
+            rows="5"
+            required
+            onChange={onChangeHandling('content')}
+            error={validators.content}
+            helperText={validators.content ? '내용을 입력하거나 확인해주세요' : ''}
+          />
+          <Divider />
+          <div className={classes.action_buttons}>
+            <Button type="button" color="secondary" aria-label="oyo-cancel" variant="outlined" onClick={goBack}>취소</Button>
+            <Button type="button" color="primary" aria-label="oyo-write" variant="contained" onClick={onSaveHandling}>저장</Button>
+          </div>
+        </Paper>
+      </div>
+    </Container>
+
   )
 }
