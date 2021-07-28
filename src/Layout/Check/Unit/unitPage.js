84bbc26d82bbb0ba538a4b17febdabad29ba7164
KJH <bkho@naver.com>
2021-07-28 19:18:32 +0900

    정검 - 옵션을 상위에서 absolute로 별도로 배치(모바일 환경 고려)

--- src/Layout/Check/Unit/unitPage.js
+++ src/Layout/Check/Unit/unitPage.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, Switch } from 'react-router';
 import { Button, Container, FormControl, FormControlLabel, FormLabel, makeStyles, Radio, RadioGroup } from '@material-ui/core'
-import { ArrowBackIos, ArrowForwardIos,  ArrowRightAlt, Shuffle } from '@material-ui/icons'
-import {red, blue, lightBlue, grey} from '@material-ui/core/colors'
+import { ArrowBackIos, ArrowForwardIos, ArrowRightAlt, Shuffle } from '@material-ui/icons'
+import { red, blue, lightBlue, grey } from '@material-ui/core/colors'
 import CheckContentComponent from './cn';
 import CheckChapterVerseComponent from './cv';
 import CategorySelect from '../../categorySelect';
 import Http from '../../../Utils/Http';
 
 const InitialOrigin = {
-  theme : "",
+  theme: "",
   bible_code: 0,
   chapter: 0,
   f_verse: 0,
   l_verse: 0,
-  content : "",
+  content: "",
 }
 const OrderType = {
-  stright : 'stright',
-  random : 'random'
+  stright: 'stright',
+  random: 'random'
 }
 const BibleVersion = {
-  kor : 'hrv',
-  gae : 'nkrv'
+  kor: 'hrv',
+  gae: 'nkrv'
 }
 var originalList = [];
 export default function UnitPageComponent(props) {
@@ -31,37 +31,61 @@ export default function UnitPageComponent(props) {
   const useStyle = makeStyles(theme => ({
     root_unit: {
       height: '100%',
+      padding: '0',
       display: 'flex',
       flexDirection: 'row',
     },
     area_content: {
       display: 'flex',
       flexDirection: 'column',
+      position: 'relative',
     },
     content_options: {
+      padding: theme.spacing(1),
       marginTop: '15px',
+      border: '1px solid',
+      borderColor: theme.palette.action.disabled,
+      borderRadius: '3px',
       display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      '& > *': {
-        maxWidth: '33%',
-      },
-      '& > * .MuiFormControlLabel-label' : {
-        display : 'flex'
+      flexDirection: 'column',
+      position: 'absolute',
+      zIndex: 10,
+      backgroundColor: grey[50],
+      left: '50%',
+      transform: 'translateX(-50%)',
+      '& > .actions' : {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        '& > *:not(:first-child)': {
+          marginTop: theme.spacing(1),
+          marginBottom: theme.spacing(1),
+        },
+        '& > *': {
+          width: '20em',
+          maxWidth: '400px'
+        },
+        '& > * .MuiFormControlLabel-label': {
+          display: 'flex'
+        }
       }
     },
-    options_select: {
-      width: '33%'
+    fold: {
+      overflow: 'hidden',
+      height: '50px',
     },
-    content_unit : {
+    options_select: {
+      // width: '33%'
+    },
+    content_unit: {
       flex: 90
     },
-    moveButton : {
+    moveButton: {
       flex: 5,
       minWidth: '0px',
       color: theme.palette.text.hint
     },
-    root_checking : {
+    root_checking: {
       display: 'flex',
       flexDirection: 'row',
       height: '100%'
@@ -91,7 +115,7 @@ export default function UnitPageComponent(props) {
       '& > div': {
         margin: '10px 0'
       },
-      '& input:read-only' : {
+      '& input:read-only': {
         backgroundColor: theme.palette.action.hover
       }
     },
@@ -99,8 +123,8 @@ export default function UnitPageComponent(props) {
       flex: 1,
       backgroundColor: theme.palette.action.hover
     },
-    succeed: { backgroundColor: blue[50], '& input': {color : theme.palette.info.main}},
-    failed: { backgroundColor: red[50], '& input': {color : theme.palette.error.main}},
+    succeed: { backgroundColor: blue[50], '& input': { color: theme.palette.info.main } },
+    failed: { backgroundColor: red[50], '& input': { color: theme.palette.error.main } },
     action_button: {
       display: 'flex',
       flexDirection: 'column',
@@ -119,18 +143,18 @@ export default function UnitPageComponent(props) {
       borderTypeColor: theme.palette.action.disabled,
       overflowY: 'auto'
     },
-    hide : {
-      display : 'none'
+    hide: {
+      display: 'none'
     },
-    correct : {
+    correct: {
       backgroundColor: lightBlue[50],
     },
-    omitted : {
+    omitted: {
       backgroundColor: grey[50],
       color: grey[500],
       textDecoration: 'line-through',
     },
-    incorrect : {
+    incorrect: {
       backgroundColor: red[50],
       color: red[500]
     },
@@ -140,12 +164,12 @@ export default function UnitPageComponent(props) {
 
   function setOriginCard(item, version) {
     setOrigin({
-      theme : item.theme,
-      bible_code : item.bible_code,
-      chapter : item.chapter,
-      f_verse : item.f_verse,
-      l_verse : item.l_verse,
-      content : (version || options.version) == BibleVersion.kor ? item.verse_kor : item.verse_gae,
+      theme: item.theme,
+      bible_code: item.bible_code,
+      chapter: item.chapter,
+      f_verse: item.f_verse,
+      l_verse: item.l_verse,
+      content: (version || options.version) == BibleVersion.kor ? item.verse_kor : item.verse_gae,
     })
   }
   async function loadRecitationCards(series_number) {
@@ -162,46 +186,47 @@ export default function UnitPageComponent(props) {
     }
   }
   const [options, setoptions] = React.useState({
-    series : '201',
-    orderType : 'stright',
-    version : 'nkrv',
-    index : 0,
+    series: '201',
+    orderType: 'stright',
+    version: 'nkrv',
+    index: 0,
   })
 
   const [origin, setOrigin] = React.useState(InitialOrigin);
   const [cardList, setCardList] = React.useState([]);
-  React.useEffect(() => {loadRecitationCards(options.series)}, [])
-  const setCardContent = function(index) {
-    if(index > -1 && index < cardList.length) {
-      setoptions({...options, index : index});
+  React.useEffect(() => { loadRecitationCards(options.series) }, [])
+  const [Fold, setFold] = useState(true)
+  const setCardContent = function (index) {
+    if (index > -1 && index < cardList.length) {
+      setoptions({ ...options, index: index });
       var item = cardList[index];
       setOriginCard(item);
     }
   }
   function shuffle(array) {
-    var currentIndex = array.length,  randomIndex;
-  
+    var currentIndex = array.length, randomIndex;
+
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-  
+
       // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
+
       // And swap it with the current element.
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-  
+
     return array;
   }
 
-  const handleOrderTypeChange = function(event) {
+  const handleOrderTypeChange = function (event) {
     let value = event.target.value;
     let shortedList = null;
-    setoptions({...options, orderType: value});
+    setoptions({ ...options, orderType: value });
 
-    if(value === OrderType.stright) {
+    if (value === OrderType.stright) {
       shortedList = cardList.sort((a, b) => a.card_num > b.card_num ? 1 : -1);
     } else {
       shortedList = shuffle(cardList);
@@ -211,51 +236,56 @@ export default function UnitPageComponent(props) {
   }
   const handleVersionChange = function (event) {
     let value = event.target.value;
-    setoptions({...options, version : value})
+    setoptions({ ...options, version: value })
     setOriginCard(cardList[options.index], value);
   }
   const handleSeriesChange = async function (event) {
     let value = event.target.value;
     await loadRecitationCards(value);
 
-    setoptions({...options, index : 0})
+    setoptions({ ...options, index: 0 })
   }
   return (
     <Container className={classes.root_unit} >
 
-      <Button type="button" color="default" className={classes.moveButton} 
-        onClick={() => {setCardContent(options.index-1)}} disabled={options.index == 0} title="이전 문제"><ArrowBackIos /></Button>
+      <Button type="button" color="default" className={classes.moveButton}
+        onClick={() => { setCardContent(options.index - 1) }} disabled={options.index == 0} title="이전 문제"><ArrowBackIos /></Button>
       <div className={classes.area_content}>
         <div className={classes.content_options}>
-          <div className={classes.options_select}><CategorySelect value={options.series} onChange={handleSeriesChange}/> </div>
-          <FormControl>
-            <FormLabel component="legend">진행 순서</FormLabel>
-            <RadioGroup row={true} value={options.orderType} onChange={handleOrderTypeChange} >
-              <FormControlLabel value={OrderType.stright} control={<Radio />} label={<ArrowRightAlt />} title="시리즈 순서" />
-              <FormControlLabel value={OrderType.random} control={<Radio />} label={<Shuffle />} title="무작위" />
-            </RadioGroup>
-          </FormControl>
-          {
-            options.series != 500 ? 
-            (
-              <FormControl >
-                <FormLabel component="legend">역본</FormLabel>
-                <RadioGroup row={true} value={options.version} onChange={handleVersionChange}>
-                  <FormControlLabel value={BibleVersion.kor} control={<Radio />} label="개역한글"></FormControlLabel>
-                  <FormControlLabel value={BibleVersion.gae} control={<Radio />} label="개정개역"></FormControlLabel>
-                </RadioGroup>
-              </FormControl>
-            ) :
-            <></>
-          }
+          <div className={`actions ${Fold ? classes.fold : ''}`}>
+            <div className={classes.options_select}>
+              <CategorySelect value={options.series} onChange={handleSeriesChange} />
+            </div>
+            <FormControl>
+              <FormLabel component="legend">진행 순서</FormLabel>
+              <RadioGroup row={true} value={options.orderType} onChange={handleOrderTypeChange} >
+                <FormControlLabel value={OrderType.stright} control={<Radio />} label={<ArrowRightAlt />} title="시리즈 순서" />
+                <FormControlLabel value={OrderType.random} control={<Radio />} label={<Shuffle />} title="무작위" />
+              </RadioGroup>
+            </FormControl>
+            {
+              options.series != 500 ?
+                (
+                  <FormControl >
+                    <FormLabel component="legend">역본</FormLabel>
+                    <RadioGroup row={true} value={options.version} onChange={handleVersionChange}>
+                      <FormControlLabel value={BibleVersion.kor} control={<Radio />} label="개역한글"></FormControlLabel>
+                      <FormControlLabel value={BibleVersion.gae} control={<Radio />} label="개정개역"></FormControlLabel>
+                    </RadioGroup>
+                  </FormControl>
+                ) :
+                <></>
+            }
+          </div>
+          <Button variant="outlined" color="default" size="small" onClick={() => setFold(!Fold)}>옵션 {Fold ? '열기' : '닫기'}</Button>
         </div>
         <Switch>
           <Route path={`${props.params ? props.params.path : ''}/check/cv`} render={props => <CheckChapterVerseComponent classes={classes} origin={origin} {...props} />} />
           <Route path={`${props.params ? props.params.path : ''}/check/cn`} render={props => <CheckContentComponent classes={classes} origin={origin} {...props} />} />
         </Switch>
       </div>
-      <Button type="button" color="default" className={classes.moveButton} 
-        onClick={() => {setCardContent(options.index+1)}} disabled={options.index == cardList.length-1} title="다음 문제"><ArrowForwardIos /></Button>
+      <Button type="button" color="default" className={classes.moveButton}
+        onClick={() => { setCardContent(options.index + 1) }} disabled={options.index == cardList.length - 1} title="다음 문제"><ArrowForwardIos /></Button>
     </Container>
   )
 }
