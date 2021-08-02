7d7d5950936d5cf8bccce7ab3fd133f9c7f580ce
KJH <bkho@naver.com>
2021-08-02 20:53:11 +0900

    Slide형 카드 목록을 한개씩 출력되는 Swipe으로 변경

--- src/Layout/Cards/CardSlide/card_html.js
+++ src/Layout/Cards/CardSlide/card_html.js
@@ -1,21 +1,20 @@
-import { makeStyles } from '@material-ui/core';
 import React from 'react'
 export default function CardHtml(props) {
 
     const [memory] = React.useState({
-        bible_code : props.item.bible_code,
-        bible_name : props.item.bible_name,
-        card_num : props.item.card_num,
-        category : props.item.category,
-        chapter : props.item.chapter,
-        f_verse : props.item.f_verse,
-        l_verse : props.item.l_verse,
-        series_code : props.item.series_code,
-        theme : props.item.theme,
-        verse_gae : props.item.verse_gae,
-        verse_kor : props.item.verse_kor,
+        bible_code: props.item.bible_code,
+        bible_name: props.item.bible_name,
+        card_num: props.item.card_num,
+        category: props.item.category,
+        chapter: props.item.chapter,
+        f_verse: props.item.f_verse,
+        l_verse: props.item.l_verse,
+        series_code: props.item.series_code,
+        theme: props.item.theme,
+        verse_gae: props.item.verse_gae,
+        verse_kor: props.item.verse_kor,
     });
-    const {classes} = props;
+    const { classes } = props;
 
     return (
         <div className={classes.carouselCard}>
@@ -31,10 +30,10 @@ export default function CardHtml(props) {
                             {memory.l_verse ? <div><span>~</span><span>{memory.l_verse}</span></div> : <></>}
                         </div>
                     </div>
-                    <div className={classes.verseText}>{memory.verse_gae}</div>
+                    <div className={classes.verseText}><div>{memory.verse_gae}</div></div>
                     <div className={classes.category}>{memory.category}</div>
                 </div>
             </div>
         </div>
     )
-} 
+}
