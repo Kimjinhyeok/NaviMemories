a96650ec27764dbb1d288bf23babd0f2d54a9c56
KJH <bkho@naver.com>
2024-03-08 16:56:52 +0900

    FIX: slide card 컴포넌트 분리 적용

--- src/Layout/Cards/CardSlide/card_html.js
+++ src/Layout/Cards/CardSlide/card_html.js
@@ -1,51 +1,96 @@
-import { Checkbox, FormControlLabel } from '@mui/material';
-import React from 'react'
-import cookies from '../../../Data/cookies';
-export default function CardHtml(props) {
-
-    const { item, classes, updatePassed, version } = props;
-
-    return (
-        <div className='w-full h-full flex justify-center items-center'>
-            <div className={'w-[300px] h-full flex justify-center items-center rounded'}>
-                <div className={'flex flex-col relative w-[400px] h-[400px] rounded border-2 p-2'}>
-                    {
-                        item.theme ?
-                            <div className={'text-xl'}>{item.theme}</div>
-                            :
-                            <></>
-                    }
-                    <div className={'mt-2 flex flex-1 flex-col whitespace-pre-wrap text-left'}>
-                        <div className={'flex flex-row align-middle mb-1 space-x-1'}>
-                            <div>{item.bible_name}</div>
-                            <div>{item.chapter}</div>
-                            <span>:</span>
-                            <div className={'flex flex-row flex-1 space-x-1'}>
-                                <span>{item.f_verse}</span>
-                                {item.l_verse ? <div><span>~</span><span>{item.l_verse}</span></div> : <></>}
-                            </div>
-                        </div>
-                        <div className={'mt-1'}><div>{version ? item.verse_gae : (item.verse_kor || item.verse_gae)}</div></div>
-                        <div className={'mt-2 text-right'}>{item.category}</div>
-                    </div>
-                    {
-                        cookies.isLogin() ?
-                            <div className={'absolute'}>
-                                <FormControlLabel
-                                    checked={(item.passed === null || item.passed === undefined) ? false : item.passed}
-                                    value={(item.passed === null || item.passed === undefined) ? false : item.passed}
-                                    control={<Checkbox color="primary" />}
-                                    label="암송"
-                                    labelPlacement="start"
-                                    onChange={(ev) => updatePassed(ev, item)}
-                                />
-                            </div>
-                            :
-                            <></>
-
-                    }
-                </div>
-            </div>
+import { Checkbox, FormControlLabel } from "@mui/material";
+import React from "react";
+import cookies from "../../../Data/cookies";
+export default function CardHtml({ item, updatePassed, version }) {
+  return (
+    <CardLayout>
+      <CardWrapper>
+        <CardTheme theme={item.theme} />
+        <div
+          className={"mt-2 flex flex-1 flex-col whitespace-pre-wrap text-left"}
+        >
+          <CardContentPosition {...item} />
+          <div className={"mt-1"}>
+            <CardContent version={version} {...item} />
+          </div>
+          <CardCategory category={item.category} />
         </div>
-    )
+        <CardOptionActions cookies={cookies} item={item} update={updatePassed}/>
+			</CardWrapper>
+    </CardLayout>
+  );
+}
+
+const CardLayout = ({ children }) => (
+  <div className="w-full h-full flex justify-center items-center">
+    <div
+      className={"w-[300px] h-full flex justify-center items-center rounded"}
+    >
+      {children}
+    </div>
+  </div>
+);
+const CardWrapper = ({children}) => (
+	<div className={"flex flex-col relative w-[400px] h-[400px] rounded border-2 p-2"}>
+		{children}
+	</div>
+)
+const CardTheme = ({ theme="" }) => (
+	theme ? <div className={"text-xl"}>{theme}</div> : <></>
+)
+const CardContentPosition = ({ bible_name="", chapter=0, f_verse=0, l_verse=0 }) => (
+	<div className={"flex flex-row items-center mt-2 mb-1 space-x-1 text-green-600"}>
+		<div>{bible_name}</div>
+		<div>{chapter}</div>
+		<span>:</span>
+		<div className={"flex flex-row flex-1 space-x-1"}>
+			<span>{f_verse}</span>
+			{l_verse ? (
+				<>
+					<span>~</span>
+					<span>{l_verse}</span>
+				</>
+			) : (
+				<></>
+			)}
+		</div>
+	</div>
+)
+const CardContent = ({version, verse_gae="", verse_kor=""}) => (
+	<div>
+		{version ? verse_gae : verse_kor || verse_gae}
+	</div>
+)
+const CardCategory = ({category}) => (
+	<div className={"mt-4 text-right text-gray-600 font-light"}>{category}</div>
+)
+const CardOptionActions = ({ cookies={}, item={}, update={} }) => {
+	
+	const handleClickUpdate = (ev) => update(ev, item)
+	return (
+		cookies.isLogin() 
+		? (
+				<div className={"absolute top-0 right-2"}>
+					<FormControlLabel
+						checked={
+							item.passed === null || item.passed === undefined
+								? false
+								: item.passed
+						}
+						value={
+							item.passed === null || item.passed === undefined
+								? false
+								: item.passed
+						}
+						control={<Checkbox color="primary" />}
+						label="암송"
+						labelPlacement="start"
+						onChange={handleClickUpdate}
+					/>
+				</div>
+			) 
+		: (
+			<></>
+		)
+	)
 }
