413a6bb1b4e294600e22350e79475dcc71f3ee75
KJH <bkho@naver.com>
2024-03-11 17:27:43 +0900

    FIX: 암송 체크 -> option 분리 및 전부 가리도록 수정

---
+++ src/Layout/Check/Unit/options.js
@@ -0,0 +1,95 @@
+import { Button, Container, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import { ArrowRightAlt, KeyboardArrowDown, KeyboardArrowUp, Shuffle } from '@mui/icons-material'
+import CategorySelect from "../../categorySelect";
+import { useState } from "react";
+import { BibleVersion, TestOrderType } from "../../../Data/common";
+
+export default function CheckOptions({ options={}, changeOptions=()=>{} }) {
+
+  const [open, setOpen] = useState(false);
+
+  const onHandleOpen = () => {
+    setOpen(!open);
+  }
+  
+  const handleOrderTypeChange = function (event) {
+    const value = event.target.value;
+    const newOptions = { ...options, orderType: value };
+    changeOptions("order", newOptions);
+
+  }
+  const handleVersionChange = function (event) {
+    const value = event.target.value;
+    const newOptions = { ...options, version: value };
+    changeOptions("version", newOptions);
+
+  }
+  const handleSeriesChange = async function (event) {
+    const value = event.target.value;
+    const newOptions = { ...options, version: value };
+    changeOptions("series", newOptions);
+  }
+  return (
+    <Container
+      maxWidth="md"
+      sx={{ display: "flex", height: "100%" }}
+      className="p-2 mt-4 border rounded-[4px] flex flex-col flex-1"
+    >
+      <div className={`flex flex-col md:flex-row space-x-0 md:space-x-4 space-y-4 md:space-y-0  actions ${open ? "overflow-hidden h-0" : "h-auto"}`}>
+        <div>
+          <CategorySelect
+            value={options.series}
+            onChange={handleSeriesChange}
+          />
+        </div>
+        <FormControl>
+          <FormLabel component="legend">진행 순서</FormLabel>
+          <RadioGroup
+            row={true}
+            value={options.orderType}
+            onChange={handleOrderTypeChange}
+          >
+            <FormControlLabel
+              value={TestOrderType.stright}
+              control={<Radio />}
+              label={<ArrowRightAlt />}
+              title="시리즈 순서"
+            />
+            <FormControlLabel
+              value={TestOrderType.random}
+              control={<Radio />}
+              label={<Shuffle />}
+              title="무작위"
+            />
+          </RadioGroup>
+        </FormControl>
+        {options.series != 500 ? (
+          <FormControl>
+            <FormLabel component="legend">역본</FormLabel>
+            <RadioGroup
+              row={true}
+              value={options.version}
+              onChange={handleVersionChange}
+            >
+              <FormControlLabel
+                value={BibleVersion.kor}
+                control={<Radio />}
+                label="개역한글"
+              ></FormControlLabel>
+              <FormControlLabel
+                value={BibleVersion.gae}
+                control={<Radio />}
+                label="개정개역"
+              ></FormControlLabel>
+            </RadioGroup>
+          </FormControl>
+        ) : (
+          <></>
+        )}
+      </div>
+      <Button onClick={onHandleOpen} startIcon={open ? <KeyboardArrowDown /> : <KeyboardArrowUp />}  size="small"  >
+        옵션 {!open ? "닫기" : "열기"}
+      </Button>
+    </Container>
+  );
+}
