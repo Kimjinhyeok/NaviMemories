0723c7069e34ddd1b01ba9c1af18b4d978f40177
KJH <bkho@naver.com>
2024-08-19 18:22:22 +0900

    FEAT: 목록 형태에도 숨김 기능 적용

--- src/Layout/Cards/CardList/card.js
+++ src/Layout/Cards/CardList/card.js
@@ -1,18 +1,24 @@
 import { Box, Card, CardContent, Checkbox, FormControlLabel, Typography } from '@mui/material'
-import React, { Fragment } from 'react'
+import React, { Fragment, useMemo } from 'react'
 import cookies from '../../../Data/cookies';
+import { useSelector } from 'react-redux';
+import { KEY_HIDE_OPTIONS } from '../../../Redux/hideOptions/action';
 
 function CardComponent (props, ref) {
     
+    const hideOptions = useSelector(state => state[KEY_HIDE_OPTIONS]);
+
     const { item, updatePassed, version, idx, length } = props;
+    const commonShow = useMemo(() => !(hideOptions.cv || hideOptions.cn), [hideOptions]);
+
     return (
         <Card ref={ref} sx={{ margin: '8px', position: 'relative' }}>
             <CardContent sx={{ display: 'flex', flexDirection: 'column', textAlign: 'left', marginTop: '4px', paddingBottom: '16px !important' }}>
-                <ThemeLayer {...item} />
-                <ChapterLayer {...item} />
-                <Box sx={{ marginTop: '4px', marginBottom: '4px' }}>{version ? item.verse_gae : (item.verse_kor || item.verse_gae)}</Box>
+                <ThemeLayer show={commonShow} {...item} />
+                <ChapterLayer show={!hideOptions.cv} {...item} />
+                <Box sx={{ marginTop: '4px', marginBottom: '4px', opacity: hideOptions.cn ? 0 : 1 }}>{version ? item.verse_gae : (item.verse_kor || item.verse_gae)}</Box>
                 <div className='w-full flex justify-between items-end'>
-                    <Box className={'mt-2 text-right text-gray-600 font-light'}>{item.category}</Box>
+                    <Box className={'h-7 mt-2 text-right text-gray-600 font-light'}>{commonShow && (item.category)}</Box>
                     <Box className='text-sm text-gray-400'>{idx+1}/{length}</Box>
                 </div>
             </CardContent>
@@ -34,29 +40,36 @@ function CardComponent (props, ref) {
         </Card>
     )
 }
-const ThemeLayer = ({theme = ''}) => (
+const ThemeLayer = ({show=true, theme = ''}) => (
     theme 
     ? 
-        <div className={'text-xl'}>
-            {theme}
+        <div className={'h-7 text-xl'}>
+            {show ? theme : ''}
         </div>
     :
         <></>
 )
-const ChapterLayer = ({bible_name = '', chapter = 0, f_verse = 0, l_verse = 0}) => (
+const ChapterLayer = ({show=true, bible_name = '', chapter = 0, f_verse = 0, l_verse = 0}) => (
     <>
-        <Box className={'mt-2 flex flex-row space-x-1 text-green-600'}>
-            <Typography sx={{ marginRight: '4px' }}>{bible_name}</Typography>
-            <Typography>{chapter}</Typography>
-            <Typography>:</Typography>
-            <Typography>{f_verse}</Typography>
+        <Box className={'h-6 mt-2 flex flex-row space-x-1 text-green-600'}>
             {
-                l_verse 
-                    ? <Fragment>
-                        ,
-                        <Typography>{l_verse}</Typography>
-                      </Fragment> 
-                    : <></>
+                show 
+                ? 
+                    <>
+                        <Typography sx={{ marginRight: '4px' }}>{bible_name}</Typography>
+                        <Typography>{chapter}</Typography>
+                        <Typography>:</Typography>
+                        <Typography>{f_verse}</Typography>
+                        {
+                            l_verse 
+                                ? <Fragment>
+                                    ,
+                                    <Typography>{l_verse}</Typography>
+                                </Fragment> 
+                                : <></>
+                        }                    
+                    </>
+                : <></>
             }
         </Box>
     </>
